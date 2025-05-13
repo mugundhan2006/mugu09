beea266aded4544b992578bddeef3aad86821fb7
mugundhan2006 <mugundhank23sit@vetias.ac.in>
2025-05-13 14:33:15 +0530

    Add files via upload

---
+++ swap-number-without-third.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    printf("Enter first number (a): ");
+    scanf("%d", &a);
+    printf("Enter second number (b): ");
+    scanf("%d", &b);
+    a = a + b;
+    b = a - b; 
+    a = a - b; 
+    printf("After swapping: a = %d, b = %d\n", a, b);
+
+    return 0;
+}
